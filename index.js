5361b9b1e67d71ee94fd0ca50b4eee45d6060a35
Jiwan Kim <j1638j@gmail.com>
2021-06-06 17:00:11 +0900

    added .gitignore and fixed some code to use env file

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 //express
 const express = require('express')
 const app = express()
 const port = 3000
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 //mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/mbtitest', { useNewUrlParser: true, useUnifiedTopology: true })
+const dbUrl = process.env.DB_URL || 'mongodb://localhost/mbtitest';
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('mongoose connected')
     }).catch(err => {
         console.log('mongoose connection failed')
     })
+
+//session
+const store = new MongoStore({
+    mongoUrl: dbUrl,
+    secret: process.env.SECRET,
+    touchAfter: 24 * 3600
+})
+
+store.on("error", e => console.log("Session store error: ", e))
+
+const sessionConfig = {
+    store,
+    name: 'session',
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig))
+
 
 //ejs
 const ejsMate = require('ejs-mate')
@@ -22,11 +54,9 @@ app.set('views', path.join(__dirname, 'views'));
 
 //statics
 app.use(express.static('public'));
-app.use(express.static('styles'));
-
 
 app.use(express.json())
-app.use(cookieParser('secret'));
+app.use(cookieParser(process.env.SECRET));
 
 //urlencoded : a method inbuilt in express to recognize the incoming Request Object as strings or arrays.
 //parse req.body 
