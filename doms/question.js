07bbb4f262022ec7db7374456930a938892aba48
Jiwan Kim <j1638j@gmail.com>
2021-06-06 17:16:59 +0900

    added process.env.PORT

--- doms/question.js
+++
@@ -1,5 +0,0 @@
-const optionButton1 = document.getElementById('option-button-1');
-
-optionButton1.addEventListener('click', function () {
-    alert('asdjpj')
-})
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== "production") {
 //express
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
